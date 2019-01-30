d49b9101bed9fd8094186f5bb82e81ccc30a1ea4
dmargalit <dmargalit@hotmail.com>
2019-01-30 19:53:49 +0200

    sandbox commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Switch, Route, Link, Redirect } from "react-router-dom";
-import { bindActionCreators } from 'redux';
-import { submit } from 'redux-form'
+import { bindActionCreators } from "redux";
+import { submit } from "redux-form";
 
-import { createCategory, deleteCategory, fetchCategory } from "../../actions/categories";
+import {
+  createCategory,
+  deleteCategory,
+  fetchCategory
+} from "../../actions/categories";
 
 class Header extends Component {
-  state = { redirect: false, submitForm: null }
+  state = { redirect: false, submitForm: null };
 
   componentDidMount() {
     // const { id } = this.props.match.params;
@@ -16,7 +20,7 @@ class Header extends Component {
 
   onDeleteClick(id) {
     deleteCategory(id);
-    this.setState({ redirect: '/categories' });
+    this.setState({ redirect: "/categories" });
   }
 
   render() {
@@ -25,35 +29,51 @@ class Header extends Component {
     const saveDeleteCategory = () => (
       <div>
         <button onClick={this.props.submitForm}>Save</button>
-        <button onClick={() => this.onDeleteClick(this.props.category.id)}>Delete</button>
+        <button onClick={() => this.onDeleteClick(this.props.category.id)}>
+          Delete
+        </button>
       </div>
     );
     const newCategory = () => (
-      <Link to="/categories/new"><button>New Category</button></Link>
+      <Link to="/categories/new">
+        <button>New Category</button>
+      </Link>
     );
     const saveDeleteLocation = () => (
       <div>
         <button onClick={this.props.submitForm}>Save</button>
-        <button onClick={() => this.onDeleteClick(this.props.category.id)}>Delete</button>
+        <button onClick={() => this.onDeleteClick(this.props.category.id)}>
+          Delete
+        </button>
       </div>
     );
     const newLocation = () => (
-      <Link to="/locations/new"><button>New Location</button></Link>
+      <Link to="/locations/new">
+        <button>New Location</button>
+      </Link>
     );
 
     if (redirect) {
-      return (<Redirect to={redirect} />);
+      return <Redirect to={redirect} />;
     } else {
       return (
         <div>
-          <h1>myLocation</h1>
-          <Route path='/categories' exact component={newCategory} />
-          <Route path='/categories/new' exact component={saveDeleteCategory} />
-          <Route path='/categories/edit/:id' exact component={saveDeleteCategory} />
+          <h1>myLocations</h1>
+          <Route path="/categories" exact component={newCategory} />
+          <Route path="/categories/new" exact component={saveDeleteCategory} />
+          <Route
+            path="/categories/edit/:id"
+            exact
+            component={saveDeleteCategory}
+          />
 
-          <Route path='/locations' exact component={newLocation} />
-          <Route path='/locations/new' exact component={saveDeleteLocation} />
-          <Route path='/locations/edit/:id' exact component={saveDeleteLocation} />
+          <Route path="/locations" exact component={newLocation} />
+          <Route path="/locations/new" exact component={saveDeleteLocation} />
+          <Route
+            path="/locations/edit/:id"
+            exact
+            component={saveDeleteLocation}
+          />
 
           <hr />
         </div>
@@ -67,9 +87,15 @@ function mapStateToProps({ category, categories }) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    submitForm: () => submit('EditCategoryForm')
-  }, dispatch);
+  return bindActionCreators(
+    {
+      submitForm: () => submit("EditCategoryForm")
+    },
+    dispatch
+  );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
